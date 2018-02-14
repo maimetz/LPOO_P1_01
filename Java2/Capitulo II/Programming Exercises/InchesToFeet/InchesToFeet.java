1254e2e3db82a7799b4294c6b9ecb2b75f691052
Metzli <torresamj.111@gmail.com>
2018-02-14 00:32:01 -0600

    LPOO_P1_01

---
+++ Java2/Capitulo II/Programming Exercises/InchesToFeet/InchesToFeet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inchestofeet;
+
+/**
+ *
+ * @author metz_
+ */
+public class InchesToFeet {
+
+    public static void main(String[] args) {
+        
+    float hg;
+    float sb;
+    hg = 74;
+    sb = (float) (74/(11.94444444));
+    System.out.printf("Pulgadas: %f\t\tPies: %f\n",hg,sb);
+    
+    } 
+}
